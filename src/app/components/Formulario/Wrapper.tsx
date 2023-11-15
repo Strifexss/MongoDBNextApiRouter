ac9aa542688c69400866d64bf90d57b5c49d42d6
Strifexss <rocombole0007@gmail.com>
2023-11-15 11:52:54 -0300

    #

--- src/app/components/Formulario/Wrapper.tsx
+++ src/app/components/Formulario/Wrapper.tsx
@@ -7,7 +7,7 @@ export default function Wrapper({children, onSubmit}:Props) {
 
     return(
         <form onSubmit={onSubmit} 
-        className="w-[25rem] h-auto p-6 bg-zinc-900 flex flex-col gap-2 rounded-md">
+        className="w-full md:w-[25rem] h-auto p-6 bg-zinc-900 flex flex-col gap-2 rounded-md">
             {children}
         </form>
     )
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,27 @@
 import {Formulario} from "./components/Formulario/index";
 import {useForm, SubmitHandler} from "react-hook-form"
 import axios from "axios";
+import {z} from 'zod'
+import { zodResolver } from "@hookform/resolvers/zod";
 
-interface FormData {
-  Email: string,
-  Password: string,
-}
+const schema = z.object({
+  Email: z.string().email("Por favor, insira um Email válido"),
+  Password: z.string().min(6, "Insira uma Senha com no minimo 5 digitos")
+})
 
 export default function Home() {
 
-  const {register, handleSubmit} = useForm<FormData>()
+  const {register, handleSubmit, formState: {errors}} = useForm({
+    mode: "all",
+    criteriaMode: "all",
+    resolver: zodResolver(schema),
+    defaultValues: {
+      Email: "",
+      Password: ""
+    }
+  })
 
-  const onSubmit: SubmitHandler<FormData> = (data) => {
+  const onSubmit: SubmitHandler<z.infer<typeof schema>> = (data) => {
 
     const Email = data.Email
     const Password = data.Password
@@ -29,10 +39,16 @@ export default function Home() {
   } 
 
   return(
-    <div className='w-screen h-screen flex justify-center items-center bg-black'>
+    <div className='w-screen h-screen flex justify-center items-center bg-black p-2'>
       <Formulario.Wrapper onSubmit={handleSubmit(onSubmit)}>
         <Formulario.FormuarioInput Type="text" PlaceHolder="Insira o seu Email" register={register("Email")}/>
+        {errors.Email && 
+        <p className="text-red-600">{errors.Email.message}</p>
+          }
         <Formulario.FormuarioInput Type="password" PlaceHolder="Insira a sua Senha" register={register("Password", {required: true})}/>
+        {errors.Password && 
+        <p className="text-red-600">{errors.Password.message}</p>
+          }
         <Formulario.ButtonForm conteudo="Logar"/>
       </Formulario.Wrapper>
     </div>
